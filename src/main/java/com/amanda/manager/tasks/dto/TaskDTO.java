5069e5a762b841a1bc50846dbc437e3f94d94459
Amanda <ans-08@hotmail.com>
2025-01-10 18:20:31 -0300

    create TaskDTO for the task manager

--- src/main/java/com/amanda/manager/tasks/dto/TaskDTO.java
+++ src/main/java/com/amanda/manager/tasks/dto/TaskDTO.java
@@ -1,4 +1,45 @@
 package com.amanda.manager.tasks.dto;
 
+
+import java.time.LocalDateTime;
+
 public class TaskDTO {
+
+    private Long id;
+    private String title;
+    private LocalDateTime deadlineDate;
+    private boolean done;
+
+    public TaskDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getDeadlineDate() {
+        return deadlineDate;
+    }
+
+    public void setDeadlineDate(LocalDateTime deadlineDate) {
+        this.deadlineDate = deadlineDate;
+    }
+
+    public boolean getDone()
+    {return  done;}
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
