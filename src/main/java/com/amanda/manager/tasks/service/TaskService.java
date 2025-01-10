12c291e5f62b1aa673b70c30ee1aea67eeecbcec
Amanda <ans-08@hotmail.com>
2025-01-10 18:22:59 -0300

    create TaskService for the task manager

--- src/main/java/com/amanda/manager/tasks/service/TaskService.java
+++ src/main/java/com/amanda/manager/tasks/service/TaskService.java
@@ -1,4 +1,90 @@
 package com.amanda.manager.tasks.service;
 
+import com.amanda.manager.tasks.entity.Task;
+import com.amanda.manager.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Service
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    //constructor injector
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+
+    public List<Task> filterByDone(boolean done) {
+        return taskRepository.findByDone(done);
+    }
+
+    public List<Task> filterByDeadlineDateBetween(LocalDateTime start, LocalDateTime end) {
+        return taskRepository.findByDeadlineDateBetween(start, end);
+    }
+
+    public List<Task> filterByStatusAndDeadline(boolean done, LocalDateTime start, LocalDateTime end) {
+        return taskRepository.filterByStatusAndDeadline(done, start, end);
+    }
+
+    public Task addTask(Task task) {
+        if (task.getTitle() == null || task.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("Task must have a title");
+        }
+
+        if (task.getDeadlineDate() != null && task.getDeadlineDate().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Deadline cannot be in the past");
+        }
+
+        return taskRepository.save(task);
+    }
+
+
+    public void removeTask(Long taskId) {
+        if (!taskRepository.existsById(taskId)) {
+            throw new IllegalArgumentException(("Task with ID") + taskId + "does not exist");
+        }
+
+        taskRepository.deleteById(taskId);
+    }
+
+    public Task updateTask(Long taskId, String title, LocalDateTime deadlineDate, Boolean done) {
+
+      Task existingTask = findById(taskId);
+
+      if (title!= null && !title.isEmpty())
+      {
+        existingTask.setTitle(title);
+      }
+
+      if (deadlineDate != null)
+      {
+          existingTask.setDeadlineDate(deadlineDate);
+      }
+
+      if (done != null)
+      {
+          existingTask.setDone(done);
+      }
+
+     return taskRepository.save(existingTask);
+
+    }
+
 }
